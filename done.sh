cd8a7c8d98d53ecffcc09ae12ed01ddaf4e2a1ee
Victor Pereira <victor.pereira@unix.cl>
2025-04-07 17:29:12 -0400

    Se modifica Android Auto por HeadUnitReloaded 7.21

--- done.sh
+++ done.sh
@@ -5,14 +5,15 @@ mount -o remount,rw /system /system
 
 #Install apks
 pm install -rtdg /mnt/udisk2/Waze.apk
-pm install -rtdg /mnt/udisk2/AA.apk
+pm install -rtdg /mnt/udisk2/HeadunitReloaded721.apk
 pm install -rtdg /mnt/udisk2/WifiManager.apk
 pm install -rtdg /mnt/udisk2/Carplay.apk
 pm install -rtdg /mnt/udisk2/AutoKit.apk
 pm install -rtdg /mnt/udisk2/BackButton.apk
 pm install -rtdg /mnt/udisk2/homebutton.apk
+pm install -rtdg /mnt/udisk2/droidinfo.apk
 
 #End with reboot
 sync
-sleep 10
+#sleep 10
 reboot
